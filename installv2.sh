7bf06217adb2e0e4318b1541ab9e1e12d3705f72
Luis Maçarico <56197240+macaricol@users.noreply.github.com>
2025-10-11 12:59:17 +0100

    Update installv2.sh

--- installv2.sh
+++ installv2.sh
@@ -210,22 +210,37 @@ install_grub(){
 #######################################################
 
 select_drive() {
-    PS3='Please enter your choice: '
-    options=("Option 1" "Option 2" "Option 3" "Quit")
-    select opt in "${options[@]}"; do
-        case $opt in
-            "Option 1"|"Option 2"|"Option 3")
-                selected_option="$opt"
-                echo "You chose: $selected_option"
-                return 0
+    HEIGHT=15
+    WIDTH=40
+    CHOICE_HEIGHT=4
+    BACKTITLE="Backtitle here"
+    TITLE="Title here"
+    MENU="Choose one of the following options:"
+    
+    OPTIONS=(1 "Option 1"
+             2 "Option 2"
+             3 "Option 3")
+    
+    CHOICE=$(dialog --clear \
+                    --backtitle "$BACKTITLE" \
+                    --title "$TITLE" \
+                    --menu "$MENU" \
+                    $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                    "${OPTIONS[@]}" \
+                    2>&1 >/dev/tty)
+    
+    clear
+    case $CHOICE in
+            1)
+                echo "You chose Option 1"
                 ;;
-            "Quit")
-                echo "Exiting." >&2
-                return 0
+            2)
+                echo "You chose Option 2"
                 ;;
-            *) echo "Invalid option $REPLY" >&2; continue;;
-        esac
-    done
+            3)
+                echo "You chose Option 3"
+                ;;
+    esac
 }
 
 set -e
