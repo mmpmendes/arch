93ba82727815659ee2be7962e9b8bb2e0b42e1bc
Luis Maçarico <56197240+macaricol@users.noreply.github.com>
2025-10-11 13:49:19 +0100

    Update post_reboot.sh

--- post_reboot.sh
+++ post_reboot.sh
@@ -6,10 +6,6 @@ sudo pacman -Syu
 # Install minimal essentials
 sudo pacman -S sddm plasma-desktop bluedevil kscreen dolphin kdegraphics-thumbnailers plasma-pa gwenview plasma-systemmonitor
 
-# Enable and start SDDM service
-sudo systemctl enable sddm
-sudo systemctl start sddm
-
 # Enable and start Bluetooth service
 sudo systemctl start bluetooth.service
 sudo systemctl enable bluetooth.service
@@ -20,5 +16,9 @@ sudo pacman -S mesa vulkan-radeon libva-mesa-driver mesa-vdpau radeontop
 
 # Install extra packages
 sudo pacman -S alacritty fastfetch mpv kdrc freerdp ttf-liberation firefox
+
+# Enable and start SDDM service
+sudo systemctl enable sddm
+sudo systemctl start sddm
 
 echo "Installation and service setup complete!"
